88c772aa035c979ca50bb9c13448f02a6bf01c68
nicoa47 <nicoadelh47@gmail.com>
2021-02-01 05:35:17 +0100

    updated readme, changed initial items

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
-# Goal Features:
+*A very basic bingo game engine.*
+
+![issue with image file](demo.png "A demo")
+
+# How to Use:
+
+- adjust parameters in user_input.js (most importantly, your items)
+- open index.html
+- click on the cells with the items that were just announced
+- enjoy your bingos! (you can continue clicking after a bingo)
+
+# (Goal) Features:
 
 - input list of words
 - set a grid size:
 	- quadratic if in three directions (horizontal, vertical, diagonal)
 	- non-quadratic --> just allow bingo in smaller direction
-- let players log in
-- check if phrase was said by comparing other players' timing of check-offs
+	- **as of now only quadratic support**
+
+## TODOs:
+
+- Multiplayer functions:
+	- let players log in
+	- check if phrase was said by comparing other players' timing of check-offs
--- user_input.js
+++ user_input.js
@@ -1,38 +1,12 @@
 // if not quadratic, then there are potentially "small" and "large" bingos
-const grid_h_n = 4;
-const grid_v_n = 4;
+const grid_h_n = 5;
+const grid_v_n = 5;
 
 // list of items
 const items = [
     ["test"],
     ["line1", "line2"],
-    ["line1", "line2", "line3"],
-    /*
-    ["her-", "gehen"],
-    ["Abgefrüh-", "stückt"],
-    ["Passt wie", "Arsch auf Eimer"],
-    ["we'll have", "to see"],
-    ["say okay", "hey"],
-    ["okay", "hey"],
-    ["That's", "se point"],
-    ["Deadline"],
-    ["Manu-", "script"],
-    ["un-", "reveal"],
-    ["money"],
-    ["Team"],
-    ["sounds", "good"],
-    ["great,", "thanks"],
-    ["business", "as usual"],
-    ["what about", "the...", "[pause]"],
-    ["GAN", "stuff"],
-    ["Lübeck"],
-    ["Buda-", "pest"],
-    ["what can", "you do"],
-    ["very", "nice"],
-    ["it hasn't", "arrived", "us/me"],
-    ["Revi-", "sion"],
-    ["Paper", "Writing"]
-    // */
+    ["line1", "line2", "line3"]
 ];
 
 // derived
