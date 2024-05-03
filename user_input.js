f8453b66f7db4e1b005c07ac526080a7331fa27b
neurodream <nico.adelhoefer@donders.ru.nl>
2024-05-03 10:42:00 +0200

    refresh state

--- user_input.js
+++ user_input.js
@@ -1,12 +1,33 @@
 // if not quadratic, then there are potentially "small" and "large" bingos
-const grid_h_n = 5;
-const grid_v_n = 5;
+const grid_h_n = 4;
+const grid_v_n = 4;
 
 // list of items
 const items = [
-    ["test"],
-    ["line1", "line2"],
-    ["line1", "line2", "line3"]
+    ["her-", "gehen"],
+    ["Abgefrüh-", "stückt"],
+    ["Passt wie", "Arsch auf Eimer"],
+    ["we'll have", "to see"],
+    ["say okay", "hey"],
+    ["okay", "hey"],
+    ["That's", "se point"],
+    ["Dead-", "line"],
+    ["Manu-", "script"],
+    ["un-", "reveal"],
+    ["money"],
+    ["Team"],
+    ["sounds", "good"],
+    ["great,", "thanks"],
+    ["business", "as usual"],
+    ["what about", "the...", "[pause]"],
+    ["GAN", "stuff"],
+    ["Lübeck"],
+    ["Buda-", "pest"],
+    ["what can", "you do"],
+    ["very", "nice"],
+    ["it hasn't", "arrived", "us/me"],
+    ["Revi-", "sion"],
+    ["Paper", "Writing"]
 ];
 
 // derived
